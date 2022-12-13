f00ae0d0cf8bf23ed7c7962acbddb489eddee7f8
omdxp <omarbelghaouti@gmail.com>
2022-12-13 05:38:23 +0100

    builder facets

---
+++ 02.Builder/02.builder-facets/main.go
@@ -0,0 +1,87 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	// address
+	StreetAddress, Postcode, City string
+
+	// employment
+	CompanyName, Position string
+	AnnualIncome          int
+}
+
+type PersonBuilder struct {
+	person *Person
+}
+
+func NewPersonBuilder() *PersonBuilder {
+	return &PersonBuilder{&Person{}}
+}
+
+type PersonAddressBuilder struct {
+	PersonBuilder
+}
+
+type PersonJobBuilder struct {
+	PersonBuilder
+}
+
+func (b *PersonBuilder) Lives() *PersonAddressBuilder {
+	return &PersonAddressBuilder{*b}
+}
+
+func (b *PersonBuilder) Works() *PersonJobBuilder {
+	return &PersonJobBuilder{*b}
+}
+
+func (b *PersonAddressBuilder) At(streetAddress string) *PersonAddressBuilder {
+	b.person.StreetAddress = streetAddress
+	return b
+}
+
+func (b *PersonAddressBuilder) In(city string) *PersonAddressBuilder {
+	b.person.City = city
+	return b
+}
+
+func (b *PersonAddressBuilder) WithPostcode(postcode string) *PersonAddressBuilder {
+	b.person.Postcode = postcode
+	return b
+}
+
+func (b *PersonJobBuilder) At(companyName string) *PersonJobBuilder {
+	b.person.CompanyName = companyName
+	return b
+}
+
+func (b *PersonJobBuilder) AsA(position string) *PersonJobBuilder {
+	b.person.Position = position
+	return b
+}
+
+func (b *PersonJobBuilder) Earning(annualIncome int) *PersonJobBuilder {
+	b.person.AnnualIncome = annualIncome
+	return b
+}
+
+func (b *PersonBuilder) Build() *Person {
+	return b.person
+}
+
+func main() {
+	pb := NewPersonBuilder()
+	pb.
+		Lives().
+		At("123 London Road").
+		In("London").
+		WithPostcode("SW12BC").
+		Works().
+		At("Fabrikam").
+		AsA("Engineer").
+		Earning(123000)
+	person := pb.Build()
+	fmt.Println(person)
+}
+
+// builder facets allow us to build a person step by step
