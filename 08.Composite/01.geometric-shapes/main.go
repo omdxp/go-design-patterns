e3342e4d837ef40dd1ef0783d8b239d32d04e446
omdxp <omarbelghaouti@gmail.com>
2022-12-13 13:39:50 +0100

    geometric shapes

---
+++ 08.Composite/01.geometric-shapes/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "strings"
+
+type GraphicObject struct {
+	Name, Color string
+	Children    []GraphicObject
+}
+
+func (g *GraphicObject) String() string {
+	sb := strings.Builder{}
+	g.print(&sb, 0)
+	return sb.String()
+}
+
+func (g *GraphicObject) print(sb *strings.Builder, depth int) {
+	sb.WriteString(strings.Repeat("*", depth))
+	if len(g.Color) > 0 {
+		sb.WriteString(g.Color + " ")
+	}
+	sb.WriteString(g.Name + "\n")
+	for _, child := range g.Children {
+		child.print(sb, depth+1)
+	}
+}
+
+func NewCircle(color string) *GraphicObject {
+	return &GraphicObject{"Circle", color, nil}
+}
+
+func NewSquare(color string) *GraphicObject {
+	return &GraphicObject{"Square", color, nil}
+}
+
+func main() {
+	drawing := GraphicObject{"My Drawing", "", nil}
+	drawing.Children = append(drawing.Children, *NewSquare("Red"))
+	drawing.Children = append(drawing.Children, *NewCircle("Yellow"))
+
+	group := GraphicObject{"Group 1", "", nil}
+	group.Children = append(group.Children, *NewCircle("Blue"))
+	group.Children = append(group.Children, *NewSquare("Blue"))
+
+	drawing.Children = append(drawing.Children, group)
+	println(drawing.String())
+}
+
+// Composite is a structural design pattern that lets you compose objects into tree structures and then work with these structures as if they were individual objects.
+
+// GraphicObject is a composite object that can contain other objects (children) and can be composed into a tree structure.
