b42534ee2b7c604d323ffb940fd9c50dfe8b37f2
omdxp <omarbelghaouti@gmail.com>
2022-12-15 14:10:04 +0100

    observer and observable

---
+++ 19.Observer/01.observer-and-observable/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+type Observable struct {
+	subs *list.List
+}
+
+func (o *Observable) Subscribe(x Observer) {
+	o.subs.PushBack(x)
+}
+
+func (o *Observable) Unsubscribe(x Observer) {
+	for z := o.subs.Front(); z != nil; z = z.Next() {
+		if z.Value.(Observer) == x {
+			o.subs.Remove(z)
+		}
+	}
+}
+
+func (o *Observable) Fire(data interface{}) {
+	for z := o.subs.Front(); z != nil; z = z.Next() {
+		z.Value.(Observer).Notify(data)
+	}
+}
+
+type Observer interface {
+	Notify(data interface{})
+}
+
+// whenever a person catches a cold,
+// a doctor must be called
+type Person struct {
+	Observable
+	Name string
+}
+
+func NewPerson(name string) *Person {
+	return &Person{
+		Observable: Observable{new(list.List)},
+		Name:       name,
+	}
+}
+
+func (p *Person) CatchACold() {
+	p.Fire(p.Name)
+}
+
+type DoctorService struct{}
+
+func (d *DoctorService) Notify(data interface{}) {
+	fmt.Printf("A doctor has been called for %s\n",
+		data.(string))
+}
+
+func main() {
+	p := NewPerson("Boris")
+	ds := &DoctorService{}
+	p.Subscribe(ds)
+
+	// let's test it!
+	p.CatchACold()
+}
+
+// Observer is a behavioral design pattern that lets you define a subscription mechanism to notify multiple objects about any events that happen to the object they’re observing.
