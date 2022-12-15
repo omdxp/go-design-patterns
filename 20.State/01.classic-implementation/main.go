c0a0dbc9cc83ba73d55cb2f901fbe93daf0eb93f
omdxp <omarbelghaouti@gmail.com>
2022-12-15 16:59:32 +0100

    classic implementation

---
+++ 20.State/01.classic-implementation/main.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+type Switch struct {
+	State State
+}
+
+func NewSwitch() *Switch {
+	return &Switch{NewOffState()}
+}
+
+func (s *Switch) On() {
+	s.State.On(s)
+}
+
+func (s *Switch) Off() {
+	s.State.Off(s)
+}
+
+type State interface {
+	On(sw *Switch)
+	Off(sw *Switch)
+}
+
+type BaseState struct {
+}
+
+func (b *BaseState) On(sw *Switch) {
+	fmt.Println("Light is already on")
+}
+
+func (b *BaseState) Off(sw *Switch) {
+	fmt.Println("Light is already off")
+}
+
+type OnState struct {
+	BaseState
+}
+
+func NewOnState() *OnState {
+	fmt.Println("Light turned on")
+	return &OnState{BaseState{}}
+}
+
+func (o *OnState) Off(sw *Switch) {
+	fmt.Println("Turning light off...")
+	sw.State = NewOffState()
+}
+
+type OffState struct {
+	BaseState
+}
+
+func NewOffState() *OffState {
+	fmt.Println("Light turned off")
+	return &OffState{BaseState{}}
+}
+
+func (o *OffState) On(sw *Switch) {
+	fmt.Println("Turning light on...")
+	sw.State = NewOnState()
+}
+
+func main() {
+	sw := NewSwitch()
+	sw.On()
+	sw.On()
+	sw.Off()
+	sw.Off()
+}
+
+// State is a behavioral design pattern that lets an object alter its behavior when its internal state changes.
+// It appears as if the object changed its class.
